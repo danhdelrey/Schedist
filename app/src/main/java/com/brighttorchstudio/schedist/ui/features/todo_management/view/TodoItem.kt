129358f711dd559fb4bf4a7ea31de6ff5fec1577
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-01-15 12:57:47 +0700

    hiển thị màu cho đọ quan trọng và ẩn/hiện chi tiết todo

--- app/src/main/java/com/brighttorchstudio/schedist/ui/features/todo_management/view/TodoItem.kt
+++ app/src/main/java/com/brighttorchstudio/schedist/ui/features/todo_management/view/TodoItem.kt
@@ -1,5 +1,6 @@
 package com.brighttorchstudio.schedist.ui.features.todo_management.view
 
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
@@ -10,16 +11,26 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowUp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconToggleButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.brighttorchstudio.schedist.config.custom_colors.importantTask
+import com.brighttorchstudio.schedist.config.custom_colors.normalTask
+import com.brighttorchstudio.schedist.config.custom_colors.trivialTask
+import com.brighttorchstudio.schedist.config.custom_colors.veryImportantTask
 import com.brighttorchstudio.schedist.config.theme.SchedistTheme
 import com.brighttorchstudio.schedist.data.todo.model.Todo
 import com.brighttorchstudio.schedist.helpers.DateTimeHelper
@@ -27,6 +38,7 @@ import java.time.LocalDateTime
 
 @Composable
 fun TodoItem(todo: Todo) {
+    var showTodoDetails by remember { mutableStateOf(false) }
     Box(
         modifier = Modifier
             .padding(horizontal = 15.dp, vertical = 5.dp)
@@ -65,43 +77,65 @@ fun TodoItem(todo: Todo) {
                         Icon(
                             imageVector = Icons.Default.DateRange,
                             contentDescription = "Calendar",
-                            modifier = Modifier.size(20.dp)
+                            modifier = Modifier.size(20.dp),
+                            tint = when(todo.priority){
+                                1 -> veryImportantTask
+                                2 -> importantTask
+                                3 -> normalTask
+                                else -> trivialTask
+                            }
                         )
                         Text(
                             text = DateTimeHelper.formatLocalDateTime(todo.dateTime),
-                            style = MaterialTheme.typography.labelLarge
+                            style = MaterialTheme.typography.labelLarge,
+                            color = when(todo.priority){
+                                1 -> veryImportantTask
+                                2 -> importantTask
+                                3 -> normalTask
+                                else -> trivialTask
+                            }
                         )
                     }
                 }
                 IconToggleButton(
-                    checked = false,
-                    onCheckedChange = { /*TODO*/ }
+                    checked = showTodoDetails,
+                    onCheckedChange = { showTodoDetails = it }
                 ) {
-                    Icon(
-                        imageVector = Icons.Default.DateRange,
-                        contentDescription = "Calendar",
-                        tint = MaterialTheme.colorScheme.outlineVariant
-                    )
+                    if(showTodoDetails){
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowUp,
+                            contentDescription = "Calendar",
+                            tint = MaterialTheme.colorScheme.outlineVariant
+                        )
+                    }else{
+                        Icon(
+                            imageVector = Icons.Default.KeyboardArrowDown,
+                            contentDescription = "Calendar",
+                            tint = MaterialTheme.colorScheme.outlineVariant
+                        )
+                    }
                 }
             }
-            Column(
-                modifier = Modifier
-                    .padding(horizontal = 14.dp)
-            ) {
-                Text(
-                    text = todo.title,
-                    style = MaterialTheme.typography.titleMedium.copy(
-                        color = MaterialTheme.colorScheme.onSurfaceVariant
-                    )
-                )
-                if (!todo.description.isNullOrEmpty())
+            AnimatedVisibility(showTodoDetails) {
+                Column(
+                    modifier = Modifier
+                        .padding(horizontal = 14.dp)
+                ) {
                     Text(
-                        text = todo.description,
-                        style = MaterialTheme.typography.bodyLarge.copy(
+                        text = todo.title,
+                        style = MaterialTheme.typography.titleMedium.copy(
                             color = MaterialTheme.colorScheme.onSurfaceVariant
                         )
                     )
+                    if (!todo.description.isNullOrEmpty())
+                        Text(
+                            text = todo.description,
+                            style = MaterialTheme.typography.bodyLarge.copy(
+                                color = MaterialTheme.colorScheme.onSurfaceVariant
+                            )
+                        )
 
+                }
             }
         }
 
