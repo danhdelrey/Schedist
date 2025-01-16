47f4b9fc984437a95736aaf6cb242b301a630f11
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-01-16 20:00:25 +0700

    styled text field

--- app/src/main/java/com/brighttorchstudio/schedist/ui/features/update_todo/view/UpdateTodoBottomSheet.kt
+++ app/src/main/java/com/brighttorchstudio/schedist/ui/features/update_todo/view/UpdateTodoBottomSheet.kt
@@ -4,15 +4,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.ModalBottomSheet
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
@@ -25,10 +22,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.brighttorchstudio.schedist.data.todo.model.Todo
 import com.brighttorchstudio.schedist.helpers.DateTimeHelper
+import com.brighttorchstudio.schedist.ui.shared_view.StyledTextField
 import java.time.LocalDateTime
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -79,48 +76,21 @@ fun UpdateTodoBottomSheet(
                         modifier = Modifier
                             .padding(horizontal = 20.dp)
                     )
-                    TextField(
+                    StyledTextField(
                         value = inputTodoTitle,
-                        textStyle = MaterialTheme.typography.titleLarge,
-                        placeholder = {
-                            Text(
-                                text = "Tên nhiệm vụ",
-                                style = MaterialTheme.typography.titleLarge,
-                                color = MaterialTheme.colorScheme.outline
-                            )
-                        },
                         onValueChange = { inputTodoTitle = it },
-                        colors = TextFieldDefaults.colors().copy(
-                            focusedContainerColor = Color.Transparent,
-                            unfocusedContainerColor = Color.Transparent,
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent
-                        ),
+                        placeholderText = "Tên nhiệm vụ",
+                        textStyle = MaterialTheme.typography.titleLarge,
                         maxLines = 2,
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .focusRequester(focusRequester)
+                        modifier = Modifier.focusRequester(focusRequester)
                     )
-                    TextField(
+
+                    StyledTextField(
                         value = inputTodoDescription,
-                        textStyle = MaterialTheme.typography.bodyLarge,
-                        placeholder = {
-                            Text(
-                                text = "Nhập mô tả cho nhiệm vụ này...",
-                                style = MaterialTheme.typography.bodyLarge,
-                                color = MaterialTheme.colorScheme.outline
-                            )
-                        },
                         onValueChange = { inputTodoDescription = it },
-                        colors = TextFieldDefaults.colors().copy(
-                            focusedContainerColor = Color.Transparent,
-                            unfocusedContainerColor = Color.Transparent,
-                            focusedIndicatorColor = Color.Transparent,
-                            unfocusedIndicatorColor = Color.Transparent
-                        ),
-                        maxLines = 3,
-                        modifier = Modifier
-                            .fillMaxWidth()
+                        placeholderText = "Nhập mô tả cho nhiệm vụ này...",
+                        textStyle = MaterialTheme.typography.bodyLarge,
+                        maxLines = 3
                     )
                 }
                 Row(
---
+++ app/src/main/java/com/brighttorchstudio/schedist/ui/shared_view/StyledTextField.kt
@@ -0,0 +1,44 @@
+package com.brighttorchstudio.schedist.ui.shared_view
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun StyledTextField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    placeholderText: String,
+    textStyle: TextStyle,
+    maxLines: Int = 1, // Mặc định là 1 dòng
+    modifier: Modifier = Modifier,
+) {
+    TextField(
+        value = value,
+        onValueChange = onValueChange,
+        textStyle = textStyle,
+        placeholder = {
+            Text(
+                text = placeholderText,
+                style = textStyle,
+                color = MaterialTheme.colorScheme.outline
+            )
+        },
+        colors = TextFieldDefaults.colors().copy(
+            focusedContainerColor = Color.Transparent,
+            unfocusedContainerColor = Color.Transparent,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        maxLines = maxLines,
+        modifier = modifier.fillMaxWidth()
+    )
+}
