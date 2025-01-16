979ca9458df2d3e1c6770eb7573ca4f66cb6db74
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-01-16 08:56:58 +0700

    remove mainscreen

--- app/src/main/java/com/brighttorchstudio/schedist/MainNavigation.kt
+++ app/src/main/java/com/brighttorchstudio/schedist/MainNavigation.kt
@@ -1,46 +1,19 @@
 package com.brighttorchstudio.schedist
 
-import androidx.compose.animation.AnimatedContentTransitionScope
-import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.brighttorchstudio.schedist.ui.features.todo_management.view.TodoManagementScreen
 
 @Composable
 fun MainNavigation() {
     val navController = rememberNavController()
-    NavHost(navController = navController, startDestination = "main_screen") {
+    NavHost(navController = navController, startDestination = "todo_management_screen") {
         composable(
-            route = "main_screen",
+            route = "todo_management_screen",
         ) {
-            MainScreen(navController = navController)
-        }
-        composable(
-            route = "screen1",
-            enterTransition = {
-                slideIntoContainer(
-                    AnimatedContentTransitionScope.SlideDirection.Up,
-                    tween(500)
-                )
-            },
-            popExitTransition = {
-                slideOutOfContainer(
-                    AnimatedContentTransitionScope.SlideDirection.Down,
-                    tween(500)
-                )
-            }
-
-        ) {
-            Scaffold {
-                innerPadding ->
-                Text(text = "Screen 1", modifier = Modifier.padding(innerPadding))
-            }
+            TodoManagementScreen(navController = navController)
         }
 
     }
--- app/src/main/java/com/brighttorchstudio/schedist/ui/features/todo_management/view/TodoManagementScreen.kt
+++ app/src/main/java/com/brighttorchstudio/schedist/ui/features/todo_management/view/TodoManagementScreen.kt
@@ -1,14 +1,19 @@
 package com.brighttorchstudio.schedist.ui.features.todo_management.view
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
@@ -26,7 +31,6 @@ import com.brighttorchstudio.schedist.ui.features.todo_management.view_model.Tod
 fun TodoManagementScreen(
     viewModel: TodoManagementViewModel = hiltViewModel(),
     navController: NavHostController,
-    innerPadding: PaddingValues
 ) {
     val uiState by viewModel.uiState.collectAsStateWithLifecycle()
     val isSelectingTodos by viewModel.isSelectingTodos.collectAsStateWithLifecycle()
@@ -66,11 +70,37 @@ fun TodoManagementScreen(
                         Button(onClick = { viewModel.deleteAllTodos() }) {
                             Text("Delete All")
                         }
-                        Button(onClick = { navController.navigate("screen1") }) {
-                            Text("Screen1")
+                    }
+                )
+            }
+        },
+        bottomBar = {
+            if (isSelectingTodos) {
+                BottomAppBar(
+                    actions = {
+                        TextButton(
+                            onClick = {
+                                viewModel.deleteSelectedTodos()
+                            }
+                        ) {
+                            Text("Delete")
                         }
                     }
                 )
+            } else {
+                NavigationBar {
+                    NavigationBarItem(
+                        selected = true,
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.Home,
+                                contentDescription = "home"
+                            )
+                        },
+                        label = { Text("Home") },
+                        onClick = { }
+                    )
+                }
             }
         }
     ) { innerPadding ->
