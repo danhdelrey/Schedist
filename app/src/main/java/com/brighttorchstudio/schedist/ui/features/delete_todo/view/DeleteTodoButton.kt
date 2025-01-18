4ea09e1f7415ee1a878a07a99f024f345edee04d
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-01-18 17:24:50 +0700

    style

--- app/src/main/java/com/brighttorchstudio/schedist/ui/features/delete_todo/view/DeleteTodoButton.kt
+++ app/src/main/java/com/brighttorchstudio/schedist/ui/features/delete_todo/view/DeleteTodoButton.kt
@@ -1,10 +1,13 @@
 package com.brighttorchstudio.schedist.ui.features.delete_todo.view
 
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
 import androidx.hilt.navigation.compose.hiltViewModel
+import com.brighttorchstudio.schedist.R
 import com.brighttorchstudio.schedist.data.todo.model.Todo
 import com.brighttorchstudio.schedist.ui.features.delete_todo.view_model.DeleteTodoViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -17,7 +20,7 @@ fun DeleteTodoButton(
     todoList: List<Todo>,
     onDeletedTodo: () -> Unit = {}
 ) {
-    TextButton(
+    IconButton(
         onClick = {
             viewModel.deleteTodos(todoList)
             viewModel.showDeletedTodoSnackbar(
@@ -27,6 +30,10 @@ fun DeleteTodoButton(
             onDeletedTodo()
         }
     ) {
-        Text("Delete")
+        Icon(
+            painter = painterResource(R.drawable.trash_alt),
+            contentDescription = null,
+            tint = MaterialTheme.colorScheme.error
+        )
     }
 }
