78e9ac1335c5a4f157af2f09103e9b1f47052b58
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-01-18 16:10:49 +0700

    style

--- app/src/main/java/com/brighttorchstudio/schedist/ui/features/manage_todo/view/ManageTodoScreen.kt
+++ app/src/main/java/com/brighttorchstudio/schedist/ui/features/manage_todo/view/ManageTodoScreen.kt
@@ -1,9 +1,12 @@
 package com.brighttorchstudio.schedist.ui.features.manage_todo.view
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
@@ -12,6 +15,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -24,10 +28,14 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
+import com.brighttorchstudio.schedist.core.helpers.UIComponentHelper
 import com.brighttorchstudio.schedist.core.navigation.BottomNavigationBar
 import com.brighttorchstudio.schedist.data.todo.model.Todo
 import com.brighttorchstudio.schedist.ui.features.delete_todo.view.DeleteTodoButton
@@ -143,51 +151,89 @@ fun ManageTodoScreen(
     ) { innerPadding ->
         when (uiState) {
             is ManageTodoViewModel.UiState.Loading -> {
-                CircularProgressIndicator(modifier = Modifier.padding(innerPadding))
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                ) {
+                    CircularProgressIndicator()
+                }
             }
 
             is ManageTodoViewModel.UiState.Success -> {
                 val todoList = (uiState as ManageTodoViewModel.UiState.Success).todoList
-                Column(
-                    modifier = Modifier
-                        .padding(innerPadding)
-                        .verticalScroll(rememberScrollState())
-                ) {
-                    todoList.forEach { todo ->
-                        TodoItem(
-                            todo = todo,
-                            isSelected = todo in selectedTodos,
-                            onToggleSelection = {
-                                selectedTodos = if (todo in selectedTodos) {
-                                    selectedTodos - todo
-                                } else {
-                                    selectedTodos + todo
-                                }
-                            },
-                            onClick = {
-                                if (isSelectionMode) {
+                if (todoList.isNotEmpty()) {
+                    Column(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .verticalScroll(rememberScrollState())
+                    ) {
+                        todoList.forEach { todo ->
+                            TodoItem(
+                                todo = todo,
+                                isSelected = todo in selectedTodos,
+                                onToggleSelection = {
                                     selectedTodos = if (todo in selectedTodos) {
                                         selectedTodos - todo
                                     } else {
                                         selectedTodos + todo
                                     }
-                                } else {
-                                    selectedTodo = todo
+                                },
+                                onClick = {
+                                    if (isSelectionMode) {
+                                        selectedTodos = if (todo in selectedTodos) {
+                                            selectedTodos - todo
+                                        } else {
+                                            selectedTodos + todo
+                                        }
+                                    } else {
+                                        selectedTodo = todo
+                                    }
+                                },
+                                isSelectionMode = isSelectionMode,
+                                onLongClick = {
+                                    isSelectionMode = true
                                 }
-                            },
-                            isSelectionMode = isSelectionMode,
-                            onLongClick = {
-                                isSelectionMode = true
-                            }
+                            )
+                        }
+                    }
+                } else {
+                    Box(
+                        contentAlignment = Alignment.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding)
+                    ) {
+                        Text(
+                            text = "Chạm vào biểu tượng + để thêm nhiệm vụ mới.",
+                            style = MaterialTheme.typography.titleLarge.copy(
+                                color = MaterialTheme.colorScheme.outline
+                            ),
+                            textAlign = TextAlign.Center,
+                            modifier = Modifier
+                                .width(200.dp)
                         )
                     }
                 }
             }
 
             is ManageTodoViewModel.UiState.Error -> {
-                Text(
-                    "Error: ${(uiState as ManageTodoViewModel.UiState.Error).message}",
-                    modifier = Modifier.padding(innerPadding)
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(innerPadding)
+                ) {
+                    CircularProgressIndicator()
+                }
+                UIComponentHelper.showSnackBar(
+                    scope = scope,
+                    snackbarHostState = snackbarHostState,
+                    message = "Có lỗi xảy ra khi tải nhiệm vụ!",
+                    actionLabel = "Đóng",
+                    onActionPerformed = {},
+                    onSnackbarDismiss = {}
                 )
             }
         }
