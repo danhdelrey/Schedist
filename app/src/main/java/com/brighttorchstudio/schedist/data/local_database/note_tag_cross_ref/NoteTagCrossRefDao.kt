c5cecbbeccaed4ba493bf2bc67d8818f5aded74b
danhdelrey <duonghoaidanh21092003@gmail.com>
2025-02-09 19:22:58 +0700

    comment & change build version

--- app/src/main/java/com/brighttorchstudio/schedist/data/local_database/note_tag_cross_ref/NoteTagCrossRefDao.kt
+++ app/src/main/java/com/brighttorchstudio/schedist/data/local_database/note_tag_cross_ref/NoteTagCrossRefDao.kt
@@ -9,22 +9,31 @@ import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteTagCrossRefDao {
+    //Annotation này đảm bảo toàn bộ thao tác được thực hiện trong một transaction.
+    // Điều này quan trọng để đảm bảo tính nhất quán của dữ liệu, đặc biệt khi truy vấn dữ liệu từ nhiều bảng.
     @Transaction
+    //Mặc dù truy vấn chỉ SELECT từ NoteEntity,
+    // Room sẽ tự động thực hiện các join cần thiết với NoteTagCrossRefEntity và TagEntity
+    // để trả về List<NoteWithTagsRef>, nhờ vào định nghĩa của NoteWithTagsRef và @Relation trong đó
     @Query("SELECT * FROM NoteEntity ORDER BY dateCreated DESC")
     fun getNotesWithTags(): Flow<List<NoteWithTagsRef>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     suspend fun insertNoteTagCrossRef(crossRef: NoteTagCrossRefEntity)
 
-    //xóa tag khỏi ghi chú cụ thể
+    //Hàm này xóa một mục từ NoteTagCrossRefEntity dựa trên noteId và tagId.
+    //Nói cách khác, nó xóa mối liên kết giữa một ghi chú cụ thể và một tag cụ thể.
     @Query("DELETE FROM NoteTagCrossRefEntity WHERE noteId = :noteId AND tagId = :tagId")
     suspend fun deleteNoteTagCrossRef(noteId: String, tagId: String)
 
-    //xóa note khỏi bảng --> gọi NoteDao để xóa luôn
+    //Hàm này xóa tất cả các mục từ NoteTagCrossRefEntity có noteId khớp với noteId được cung cấp.
+    //Nói cách khác, nó xóa tất cả các tag "liên kết" với một ghi chú cụ thể.
+    //Lưu ý rằng hàm này chỉ xóa các mục trong bảng trung gian, chứ không xóa bản thân ghi chú.
+    //Bạn cần gọi một hàm khác trong NoteDao để xóa ghi chú khỏi bảng NoteEntity.
     @Query("DELETE FROM NoteTagCrossRefEntity WHERE noteId = :noteId")
     suspend fun deleteNoteTagCrossRefsByNoteId(noteId: String)
 
-    //thêm một tag vào một ghi chú cụ thể
+    //liên kết 1 tag với 1 note
     @Transaction
     suspend fun addTagToNote(noteId: String, tagId: String) {
         insertNoteTagCrossRef(NoteTagCrossRefEntity(noteId, tagId))
--- app/src/main/java/com/brighttorchstudio/schedist/data/local_database/note_tag_cross_ref/NoteTagCrossRefEntity.kt
+++ app/src/main/java/com/brighttorchstudio/schedist/data/local_database/note_tag_cross_ref/NoteTagCrossRefEntity.kt
@@ -9,15 +9,21 @@ import com.brighttorchstudio.schedist.data.local_database.tag.TagEntity
 
 @Entity(primaryKeys = ["noteId", "tagId"])
 data class NoteTagCrossRefEntity(
-    val noteId: String,
-    val tagId: String
+    val noteId: String, // Khóa ngoại tham chiếu đến noteId trong bảng NoteEntity
+    val tagId: String // Khóa ngoại tham chiếu đến tagId trong bảng TagEntity
 )
 
+// Đây không phải là một entity được lưu trữ trong database
+// Nó là một lớp dữ liệu được sử dụng để truy xuất dữ liệu từ nhiều bảng liên quan
+// Nó cho phép lấy một NoteEntity cùng với tất cả các TagEntity liên quan
 data class NoteWithTagsRef(
+    //@Embedded cho phép nhúng một entity khác (ở đây là NoteEntity) vào lớp này
+    // Vì vậy, NoteWithTagsRef sẽ chứa tất cả các trường của NoteEntity
     @Embedded val note: NoteEntity,
     @Relation(
-        parentColumn = "noteId",
-        entityColumn = "tagId",
+        parentColumn = "noteId", //Chỉ định cột trong entity cha (NoteEntity) được sử dụng trong mối quan hệ
+        entityColumn = "tagId", //Chỉ định cột trong entity con (TagEntity) được sử dụng trong mối quan hệ
+        //Junction cho biết Room sẽ sử dụng bảng NoteTagCrossRefEntity để tìm ra mối quan hệ giữa NoteEntity và TagEntity
         associateBy = Junction(NoteTagCrossRefEntity::class)
     )
     val tags: List<TagEntity>
