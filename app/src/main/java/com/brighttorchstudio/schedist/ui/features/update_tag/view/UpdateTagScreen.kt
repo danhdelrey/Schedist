dc3e6728fb42ae8a94d1b8d95d6d48946ed9629f
Thanh Dat <thanhdatphan725@gmail.com>
2025-02-14 14:22:16 +0700

    flowrow

--- app/src/main/java/com/brighttorchstudio/schedist/ui/features/update_tag/view/UpdateTagScreen.kt
+++ app/src/main/java/com/brighttorchstudio/schedist/ui/features/update_tag/view/UpdateTagScreen.kt
@@ -1,24 +1,42 @@
 package com.brighttorchstudio.schedist.ui.features.update_tag.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.brighttorchstudio.schedist.R
+import com.brighttorchstudio.schedist.core.common.TagColor
 import com.brighttorchstudio.schedist.data.tag.model.Tag
 import com.brighttorchstudio.schedist.ui.features.update_tag.view_model.UpdateTagViewModel
 
-@OptIn(ExperimentalMaterial3Api::class)
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalLayoutApi::class)
 @Composable
 fun UpdateTagScreen(
     viewModel: UpdateTagViewModel = hiltViewModel(),
@@ -60,6 +78,47 @@ fun UpdateTagScreen(
             )
         }
     ) { innerPadding ->
-        Text("hello", modifier = Modifier.padding(innerPadding))
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+        ) {
+            Text("Mau")
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                ,
+                contentAlignment = Alignment.Center
+
+            ) {
+                FlowRow(
+                    horizontalArrangement = Arrangement.spacedBy(10.dp),
+                    verticalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    enumValues<TagColor>().forEach {
+                        Box(
+                            modifier = Modifier
+                                .clip(
+                                    shape = CircleShape
+                                )
+                                .background(
+                                    color = it.color
+
+                                )
+                                .size(44.dp)
+                                .clickable(
+                                    onClick = {}
+                                )
+                                .border(
+                                    width = 4.dp,
+                                    color = MaterialTheme.colorScheme.primary,
+                                    shape = CircleShape
+                                )
+                        ) {
+
+                        }
+                    }
+                }
+            }
+        }
     }
 }
